bd5bd3254dd41a5744c17231194408b35bff8bfb
nordiws <nordiws@gmail.com>
2020-11-10 19:22:40 -0300

    Updated CORS autorized address

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://nordiws-grades-app.herokuapp.com/',
   })
 );
 app.use('/', gradesRouter);
